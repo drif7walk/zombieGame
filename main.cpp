81e1250752622073b34cc7c2c67f10a41d7b6b0b
drif7walk <drif7walk@gmail.com>
2014-10-23 23:32:31 +0300

    hl.exe

--- main.cpp
+++ main.cpp
@@ -1,7 +1,57 @@
 #include <SDL2/SDL.h>
+using namespace std;
 
-int main(void)
+int main(int argc, char** argv)
 {
-	int foo = 0;
-	return foo;
+	SDL_Event e;
+	bool quit = false;
+	
+	/* XXX: Tā vietā, lai lādētu _visu_ pēcāk sadalīt šo if
+	vairākos if-os, kas salādē tikai izmantotos SDL modules. */
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		return 1;
+	}
+
+	/* Izveidot logu. */
+	SDL_Window* window = SDL_CreateWindow("hl2.exe", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+
+	/* C++11 */
+	if (window == nullptr)
+	{
+		SDL_Quit();
+	}
+
+	/* Rendereris */
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	double _fps = 1000 / 60.0f;
+
+	
+
+	while(!quit)
+	{
+		while(SDL_PollEvent(&e))
+		{
+			switch(e.type)
+			{
+				case SDL_QUIT:
+					quit = true;
+				default: continue;	
+			}
+		}
+
+		//renderer->Render(a, b, c);
+	
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+
+		SDL_Delay(_fps);
+		
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
 }
