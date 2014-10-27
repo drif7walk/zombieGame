c84f8c927f0d3be268d5c9ef515aed0bf30c82b3
drif7walk <drif7walk@gmail.com>
2014-10-27 17:08:46 +0200

    crunchbang

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <SDL2/SDL.h>
+#include <fstream>
+#include <string>
+#include <map>
+#include <stdlib.h>
+#include "sprite.h"
+
 using namespace std;
+
+void LoadSpritesFromList(SDL_Renderer*, map<string, Sprite*>);
 
 int main(int argc, char** argv)
 {
@@ -27,31 +35,123 @@ int main(int argc, char** argv)
 
 	double _fps = 1000 / 60.0f;
 
-	
+	/* Load assets */
+	Sprite* guy = new Sprite("guy.bmp", renderer);
+	/* XXX */
+	guy->rows = 4;
+	guy->cols = 3;
+	guy->framewidth = guy->w / guy->cols;
+	guy->frameheight = guy->h / guy->rows;
+
+	map<string, Sprite*> sprites;
+	/* End load assets */
+
+	LoadSpritesFromList(renderer, sprites);
+
+
 
 	while(!quit)
 	{
+		const Uint8* keybuf = SDL_GetKeyboardState(NULL);
+
 		while(SDL_PollEvent(&e))
 		{
 			switch(e.type)
 			{
 				case SDL_QUIT:
 					quit = true;
+					break;
 				default: continue;	
 			}
+		}
+
+		/* Update */
+		if (keybuf[SDL_SCANCODE_W])
+		{
+			guy->y += -3.142;
+			guy->AnimateStep(2);
+		}
+		if (keybuf[SDL_SCANCODE_A])
+		{
+			guy->x += -3.142;
+			guy->AnimateStep(3);
+		}
+		if (keybuf[SDL_SCANCODE_S])
+		{
+			guy->y += 3.142;
+			guy->AnimateStep(0);
+		}
+		if (keybuf[SDL_SCANCODE_D])
+		{
+			guy->x += 3.142;
+			guy->AnimateStep(1);
 		}
 
 		//renderer->Render(a, b, c);
 	
 		SDL_RenderClear(renderer);
+
+		/* XXX */
+		sprites["guy"]->Render(renderer);
+
 		SDL_RenderPresent(renderer);
 
 		SDL_Delay(_fps);
 		
 	}
 
+	delete guy;
+
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
+}
+
+void LoadSpritesFromList(SDL_Renderer* ren, map<string, Sprite*> sprmap)
+{
+	
+	/* open config then parse config */
+	string s;
+	ifstream conffile("sprites.list");
+
+	if (conffile.is_open()) // File exists
+	{
+		while (getline(conffile, s))
+		{
+			/* C++ does not do switch on strings, use if/else if/else if */
+			if (s[0] == '#') continue; // comment
+
+			if (s.compare("@STARTSPRITE"))
+			{
+				Sprite* spr;
+
+				string inln;
+				getline(conffile, inln);	
+				spr = new Sprite(inln, ren);
+				
+				getline(conffile, spr->name);
+
+				getline(conffile, inln);
+				spr->rows = atoi(inln.c_str());
+
+				getline(conffile, inln);
+				spr->cols = atoi(inln.c_str());
+
+				sprmap.insert( pair<string, Sprite*>(spr->name, spr) );
+			}
+
+			/* etc.  */
+
+
+
+		}
+
+		conffile.close();
+	}
+	else // File does not exist
+	{
+		/* PANIC */
+	}
+
 }
