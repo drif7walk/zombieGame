3fd72e5c15f00955e3a52f764a9c899ed8567dc2
drif7walk <drif7walk@gmail.com>
2014-10-29 14:49:47 +0200

    zombie added

---
+++ zombie.hpp
@@ -0,0 +1,28 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+using namespace std;
+
+class Zombie: public Sprite {
+
+private:
+
+	double velocity = 12;
+
+public:
+
+	void Update(double deltaTime) override 
+	{
+		Sprite::Update(deltaTime);
+
+
+	}
+
+    Zombie(string filename, SDL_Renderer* ren)
+        : Sprite(filename, ren)
+    {
+        ;;
+    }
+
+};
+#endif
